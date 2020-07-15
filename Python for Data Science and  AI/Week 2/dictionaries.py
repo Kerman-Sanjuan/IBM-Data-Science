e702278cf1e5618565b5f4d8e409b0380d1e9044
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2020-07-16 00:58:06 +0200

    Uptade week 2

---
+++ Python for Data Science and  AI/Week 2/dictionaries.py
@@ -0,0 +1,10 @@
+#Type of collection
+#Instead of using index, they use "keys"
+#They keys cannot be duplicated and are unique.
+Dictionary = {
+    "nombres":["kerman","janire"],
+    "edad": [1,2],
+    "donde_viven":["Algorta", "Plen"]
+}
+
+print(Dictionary[edad])
