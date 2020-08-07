ef552c683cc5ddb0f770ab96c2dd9d38b3e2ae9f
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2020-08-08 01:19:22 +0200

    Major UPDATE

--- Python for Data Science and  AI/Week 4/write_files.py
+++
@@ -1,6 +0,0 @@
-texto = ["hola \n","que tal \n","estass!!!!!"]
-ejemplo = "D://Desktop/MachineLearning/IBM/prueba_escritura.txt"
-with open(ejemplo,"a") as file1:
-    file1.write("Esto esta escrito con VSCode " )
-    for palabras in texto: #En este modo escribe en el texto.
-        file1.write(palabras)
