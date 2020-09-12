eace0d1f98a858c7cb32315f3729b83bd727a282
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2020-09-12 23:55:33 +0200

    final update

---
+++ Python for Data Science and  AI/Week 2/List_and_tuples.py
@@ -0,0 +1,8 @@
+#Lists and tuples.
+import numpy as np
+
+#Tuples are ordered sequence, separated with commas and parentheses.
+tupla = (1,2,3,4,5,6,7,8,9,10)
+tupla_multiple = (1,"kerman",3.1415,True)
+print(tupla_multiple[1])
+print(tupla_multiple[-1])
