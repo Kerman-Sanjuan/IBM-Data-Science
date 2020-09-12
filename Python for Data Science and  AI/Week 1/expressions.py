5a9e80737ff14bf62c3183cc33b1be309ba6eb27
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2020-09-13 00:01:15 +0200

    update

---
+++ Python for Data Science and  AI/Week 1/expressions.py
@@ -0,0 +1,13 @@
+#Mathematical operations
+print(1+2)
+print(235+343)
+print(23*4)
+#Division
+#Full division
+print(11//5)
+#Decimal division
+print(11/5)
+#Declaration of variables
+v = 5
+v +=1
+b = 2
---
+++ Python for Data Science and  AI/Week 1/string_operations.py
@@ -0,0 +1,19 @@
+Name = "Michael Jackson"
+print(Name[0])
+print(Name[-1]) #Las one
+#Divide a number
+ic = Name[1:3]
+n = len(Name)
+#Sum two strings
+surname = Name+" Sanjuan"
+
+#New line
+print("Hola \n xd ")
+
+#Tab
+print("Hola \t xd ")
+#BackSlash
+print("Hola \\ xd ")
+#Stride
+print(Name.find('el'))
+#Embed link: https://labs.cognitiveclass.ai/tools/jupyterlab/lab/tree/labs/PY0101EN/PY0101EN-1-2-Strings.ipynb
